99f390fc5efaf08441b6a5a13f92ee34de8fde79
Dinesh Kumar <46622904+dine9936@users.noreply.github.com>
2022-10-06 06:50:11 +0530

    My First ktor server

---
+++ src/main/kotlin/com/example/Application.kt
@@ -0,0 +1,11 @@
+package com.example
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.example.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
+        configureRouting()
+    }.start(wait = true)
+}
